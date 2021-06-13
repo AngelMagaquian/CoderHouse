34f4eb0bb8c03010df325e01b89b83362deb7cc9
AngelMagaquian <69281069+AngelMagaquian@users.noreply.github.com>
2021-06-13 14:24:03 -0300

    Problems

--- magaquianecommerce/src/components/ItemList/ItemList.js
+++ magaquianecommerce/src/components/ItemList/ItemList.js
@@ -2,46 +2,28 @@ import React from 'react'
 import './ItemList.css';
 import Item from '../Item/Item';
 
-function ItemList() {
+function ItemList(props) {
 
-    const items =[
-        {
-            name: 'Item 1',
-            image: 'https://www.mayoristastecno.com.ar/wp-content/uploads/2021/04/1000x1000-Fondo-Blanco-68-200x200.jpg',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            price: '200',
-            stock: 10
-        }
-    ];
+   
+    console.log(props);
 
-    const promise = new Promise ((resolve, reject) =>{
-            resolve(items)
-    });
 
-    return(
-        promise.then(
-            data =>{
-                    return(
-                        data.map((e)=>
-                        {<Item 
-                            name ={e.name}
-                            image ={e.image}
-                            desc ={e.desc}
-                            price ={e.price}
-                            stock ={e.stock}/>}
-                        )
-                    );
-                
-            }, error =>{
-                console.log(error);
-            }
-            ).catch(error =>{
-                alert('NO HAY STOCK' +error);
-            })
-    )
-    
-    
-    
+   return(
+       
+        props.map((e) =>{
+            <Item 
+            name = {e.name}
+            image ={e.image}
+            desc ={e.desc}
+            price ={e.price}
+            stock ={e.stock} 
+        />
+        })
+   )
+            
 }
+    
+
+
 
 export default ItemList
--- magaquianecommerce/src/components/ItemListContainer/ItemListContainer.js
+++ magaquianecommerce/src/components/ItemListContainer/ItemListContainer.js
@@ -7,34 +7,52 @@ import ItemList from '../ItemList/ItemList';
 
 
 const ItemListContainer = () => {
+    const [ItemState, setItem] = useState([]);
+
+    const items =[
+        {
+            name: 'Item 1',
+            image: 'https://www.mayoristastecno.com.ar/wp-content/uploads/2021/04/1000x1000-Fondo-Blanco-68-200x200.jpg',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            price: '200',
+            stock: 10
+        }
+    ];
+
+    useEffect(()=>{
+        const promise = new Promise ((resolve, reject) =>{
+            resolve(items)
+            console.log('promise '+items)
+        });
+    
+        promise.then(data =>{
+            if(data){
+                setItem(data);
+            }else{
+                throw new Error('error');
+            }
+            
+
+        }, error =>{
+            console.log(error);
+        }
+        ).catch(error =>{
+            alert('NO HAY STOCK' +error);
+        })
+    }, []);
+    
+
+    
+    
     return (
         <div className="ItemList-container container">
             
             <div className="row">
                 <div className="col-12">
-                    <h1 className="ItemList-title">HOLO</h1>
+                    <h1 className="ItemList-title"></h1>
                 </div>
-
-                {/* <div className="col-12">
-                    <div className="row">
-                        {itemList.map((e) => <Item 
-                            name ={e.name}
-                            image ={e.image}
-                            desc ={e.desc}
-                            price ={e.price}
-                            stock ={e.stock}
-                        />
-                        )} 
-                          
-                    </div>
-                   
-                </div> */}
-
-               <ItemList/>
-
+                <ItemList data = {ItemState}/>
             </div>
-            
-            
         </div>
     )
 }
